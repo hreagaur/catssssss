0a957a5348245a8866e34c27ac097bcf9290173e
NihalAmin <nihalamin16@gmail.com>
2023-11-20 23:46:30 +0530

    gb

---
+++ Main_Script.js
@@ -0,0 +1,6 @@
+function onclisons() {
+    $("#intro-containers").removeClass("d-block");
+    $("#intro-containers").addClass("d-none");
+    $("#login-content").removeClass("d-none");
+    $("#login-content").addClass("d-block");
+}
